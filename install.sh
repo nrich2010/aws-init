78a7d145d503a48ffd043ce41243a88347389724
Nick Rich <nick@nrwebdev.com>
2013-03-08 15:31:32 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#manual --
+wget http://nginx.org/keys/nginx_signing.key
+sudo apt-key add nginx_signing.key
+
+codename=`lsb_release -c -s`
+echo "deb http://nginx.org/packages/ubuntu/ $codename nginx
+deb-src http://nginx.org/packages/ubuntu/ $codename nginx"
+apt-get update
+apt-get install nginx
+#end manual
+
+----
+#!/bin/bash
+set -e -x
+
+#prepare instance
+apt-get -q -y install python-software-properties
+wait
+apt-get -q -y update
+wait
+
+#install nginx
+add-apt-repository ppa:nginx/stable
+wait
+apt-get -q -y update && apt-get install nginx
+wait
+
+#install mysql
+DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install mysql-server mysql-client
+wait
+#sudo mysql_secure_installation
+
+#install memcached
+apt-get -q -y install memcached
+wait
+
+#install php
+apt-get -q -y install php5-cli php5-common php5-mysql php5-suhosin php5-gd php5-fpm php5-cgi php-pear php5-memcache php-apc php5-geoip php5-curl php5-dev php5-mhash php5-pgsql php5-sqlite php5-tidy php5-xmlrpc php5-mcrypt php5-imagick php5-ffmpeg
+wait
+
+#configure services

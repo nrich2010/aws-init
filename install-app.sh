e20683abdc255dc011d47ba32c8397faa093099d
Nick Rich <nick@nrwebdev.com>
2013-05-17 09:26:11 -0400

    Create install-app.sh
    separate services

---
+++ install-app.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -x
+
+#prepare instance
+apt-get -q -y install python-software-properties
+wait
+apt-get -q -y update
+wait
+
+#install mysql client
+DEBIAN_FRONTEND=noninteractive
+apt-get -q -y install mysql-client
+wait
+
+#install memcached
+apt-get -q -y install memcached
+wait
+
+#install php
+apt-get -q -y install php5-cli php5-common php5-mysql php5-suhosin php5-gd php5-fpm php5-cgi php-pear php5-memcache php-apc php5-geoip php5-curl php5-dev php5-mhash php5-pgsql php5-sqlite php5-tidy php5-xmlrpc php5-mcrypt php5-imagick php5-ffmpeg
+wait
+
+#configure services
